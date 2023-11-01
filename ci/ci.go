bbf7d02c797fb2287338b41a5085346fb8e37d46
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-11-01 01:26:12 +0100

    finish ci pipeline
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- ci/ci.go
+++ ci/ci.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"golang.org/x/sync/errgroup"
 )
@@ -9,6 +10,7 @@ import (
 const (
 	goVersion           = "1.21.3"
 	golangciLintVersion = "1.54.2"
+	artifactBaseImage   = "alpine:3.18.4"
 )
 
 type Ci struct{}
@@ -18,9 +20,17 @@ func (m *Ci) CI(ctx context.Context) error {
 	var group errgroup.Group
 
 	// Build
+	var app *Container
 	{
 		dag := dag.Pipeline("Build")
-		_ = dag
+
+		group.Go(func() error {
+			var err error
+
+			app, err = build(dag).Sync(ctx)
+
+			return err
+		})
 	}
 
 	// Test
@@ -28,7 +38,9 @@ func (m *Ci) CI(ctx context.Context) error {
 		dag := dag.Pipeline("Test")
 
 		group.Go(func() error {
-			return testAll(ctx, dag)
+			_, err := test(dag).Sync(ctx)
+
+			return err
 		})
 	}
 
@@ -37,24 +49,110 @@ func (m *Ci) CI(ctx context.Context) error {
 		dag := dag.Pipeline("Lint")
 
 		group.Go(func() error {
-			return lintAll(ctx, dag)
+			_, err := lint(dag).Sync(ctx)
+
+			return err
 		})
 	}
 
-	return group.Wait()
+	err := group.Wait()
+	if err != nil {
+		return err
+	}
+
+	// TODO: remove this from the initial version and add back later
+	scanOutput, err := dag.Trivy().ScanContainer(ctx, app)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(scanOutput)
+
+	// If this is a release, publish and deploy the container image
+	if false {
+		ref, err := app.Publish(ctx, "registry.example.com/app")
+		if err != nil {
+			return err
+		}
+
+		// Deploy ref
+		// TODO(mark): deploy to Fly.io
+		_ = ref
+	}
+
+	return nil
 }
 
-// Build jobs
-func (m *Ci) Build() *Build {
-	return &Build{}
+// Build the container image
+func (m *Ci) Build() *Container {
+	return build(dag)
+}
+
+func build(dag *Client) *Container {
+	host := dag.Host()
+
+	binary := dag.Container().
+		From(fmt.Sprintf("golang:%s", goVersion)).
+		WithMountedCache("/root/.cache/go-build", dag.CacheVolume("go-build")).
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod")).
+		WithMountedDirectory("/src", host.Directory(root(), HostDirectoryOpts{
+			Exclude: []string{".direnv", ".devenv"},
+		})).
+		WithWorkdir("/src").
+		WithEnvVariable("CGO_ENABLED", "0").
+		WithEnvVariable("GOOS", "linux").
+		WithEnvVariable("GOARCH", "amd64").
+		WithExec([]string{"go", "build", "-ldflags", "-X main.version=${VERSION}", "-o", "/usr/local/bin/app", "."}).
+		File("/usr/local/bin/app")
+
+	return dag.Container().From(artifactBaseImage).
+		WithExec([]string{"apk", "add", "--update", "--no-cache", "ca-certificates", "tzdata"}).
+		WithFile("/usr/local/bin/app", binary, ContainerWithFileOpts{
+			Permissions: 0555,
+		}).
+		WithExposedPort(8080).
+		WithEntrypoint([]string{"app"})
 }
 
 // Test jobs
-func (m *Ci) Test() *Test {
-	return &Test{}
+func (m *Ci) Test() *Container {
+	return test(dag)
+}
+
+func test(dag *Client) *Container {
+	host := dag.Host()
+
+	return dag.Container().
+		From(fmt.Sprintf("golang:%s", goVersion)).
+		WithMountedCache("/root/.cache/go-build", dag.CacheVolume("go-build")).
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod")).
+		WithMountedDirectory("/src", host.Directory(root(), HostDirectoryOpts{
+			Exclude: []string{".direnv", ".devenv", "api/client/node/node_modules"},
+		})).
+		WithWorkdir("/src").
+		WithExec([]string{"go", "test", "-v", "./..."})
 }
 
 // Linter jobs
-func (m *Ci) Lint() *Lint {
-	return &Lint{}
+func (m *Ci) Lint() *Container {
+	return lint(dag)
+}
+
+func lint(dag *Client) *Container {
+	host := dag.Host()
+
+	bin := dag.Container().
+		From(fmt.Sprintf("docker.io/golangci/golangci-lint:v%s", golangciLintVersion)).
+		File("/usr/bin/golangci-lint")
+
+	return dag.Container().
+		From(fmt.Sprintf("golang:%s", goVersion)).
+		WithMountedCache("/root/.cache/go-build", dag.CacheVolume("go-build")).
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod")).
+		WithMountedDirectory("/src", host.Directory(root(), HostDirectoryOpts{
+			Exclude: []string{".direnv", ".devenv", "api/client/node/node_modules"},
+		})).
+		WithWorkdir("/src").
+		WithFile("/usr/local/bin/golangci-lint", bin).
+		WithExec([]string{"golangci-lint", "run", "--verbose"})
 }
